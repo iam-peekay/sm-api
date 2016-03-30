51a7f0a682914546d9663539c852a51df5ee86e0
iam-peekay <iam.preethi.k@gmail.com>
2016-03-30 16:26:21 -0700

    Fix bug in nock testing

--- connectors/tests/__GMConnectorTests__.js
+++ connectors/tests/__GMConnectorTests__.js
@@ -1,20 +1,20 @@
 const chai = require('chai');
-const dirtyChai = require('dirty-chai');
 const expect = chai.expect;
 const should = chai.should();
 const sinon = require('sinon');
 const nock = require('nock');
 const GMConnector = require('./../GM_connector');
 const nockGetVehicleInfo = require('./nock/nockGetVehicleInfo');
-const nockGetSecurityInfo = require('./nock/nockGetVehicleInfo');
+const nockGetSecurityInfo = require('./nock/nockGetSecurityInfo');
+const errorMessages = require('./../../utils/errors/messages');
 
 describe('GM connector', function() {
 
-  describe('getVehicleInfo', function () {
-    var gmScope, gmConnector;
+  describe('POST /getVehicleInfo - success', function () {
+    var gmScope, gmConnector, response;
 
     before(function() {
-      gmScope = nock('http://gmapi.azurewebsites.net').persist();
+      gmScope = nock('http://gmapi.azurewebsites.net');
       gmConnector = new GMConnector();
       gmScope
         .post('/getVehicleInfoService', {
@@ -23,7 +23,34 @@ describe('GM connector', function() {
         })
         .matchHeader('Content-Type', 'application/json')
         .reply(200, nockGetVehicleInfo.success);
+    });
 
+    after(function() {
+      nock.cleanAll();
+    });
+
+    context('when correct id and responseType is passed in', function() {
+      it('should return 200 response with vehicle info', function() {
+        return gmConnector._getVehicleInfo({ "id": "1234"})
+                .then(function(response) {
+                  response = response;
+                  expect(response.status).to.equal('200');
+                  expect(response.data.vin.value).to.equal('123123412412');
+                  expect(response.data.color.value).to.equal('Metallic Silver');
+                  expect(response.data.fourDoorSedan.value).to.equal('True');
+                  expect(response.data.twoDoorCoupe.value).to.equal('False');
+                  expect(response.data.driveTrain.value).to.equal('v8');
+                });
+      });
+    });
+  });
+
+  describe('POST /getVehicleInfo - error', function () {
+    var gmScope, gmConnector, response;
+
+    before(function() {
+      gmScope = nock('http://gmapi.azurewebsites.net');
+      gmConnector = new GMConnector();
       gmScope
         .post('/getVehicleInfoService', {
           "id": "1239",
@@ -37,34 +64,20 @@ describe('GM connector', function() {
       nock.cleanAll();
     });
 
-    describe('POST /getVehicleInfo', function() {
-
-      context('when correct id and responseType is passed in', function() {
-        it('should return 200 response with vehicle info', function() {
-          gmConnector._getVehicleInfo({ "id": "1234"})
-            .then(function(response) {
-              expect(response.status).to.equal('200');
-              expect(response.data.vin.value).to.equal('123123412412');
-              expect(response.data.color.value).to.equal('Metallic Silver');
-              expect(response.data.fourDoorSedan.value).to.equal('True');
-              expect(response.data.twoDoorCoupe.value).to.equal('False');
-              expect(response.data.driveTrain).to.equal('v8');
-            });
-        });
-      });
-
-      context('when incorrect id is passed in', function() {
-        it('should return 404 response with error', function() {
-          gmConnector._getVehicleInfo({ "id": "1239"})
-            .then(function(response) {
-              expect(response.status).to.equal('404');
-            });
-        });
+    context('when incorrect id is passed in', function() {
+      it('should return 404 response', function() {
+        return gmConnector._getVehicleInfo({ "id": "1239"})
+                .then(function() {
+                })
+                .catch(function(error) {
+                  expect(error.error.status).to.equal(404);
+                  expect(error.message).to.equal(errorMessages.oemRequestError);
+                });
       });
     });
   });
 
-  describe('getSecurityInfo', function () {
+  describe('POST /getSecurityStatusService - success', function () {
     var gmScope, gmConnector;
 
     before(function() {
@@ -77,7 +90,31 @@ describe('GM connector', function() {
         })
         .matchHeader('Content-Type', 'application/json')
         .reply(200, nockGetSecurityInfo.success);
+    });
 
+    after(function() {
+      nock.cleanAll();
+    });
+
+    context('when correct id and responseType is passed in', function() {
+      it('should return 200 response with security info', function() {
+        return gmConnector._getSecurityInfo({ "id": "1234"})
+                .then(function(response) {
+                  expect(response.status).to.equal('200');
+                  expect(response.data.doors.values.length).to.equal(2);
+                  expect(response.data.doors.values[0]).to.have.all.keys('location', 'locked');
+                  expect(response.data.doors.values[1]).to.have.all.keys('location', 'locked');
+                });
+      });
+    });
+  });
+
+  describe('POST /getSecurityStatusService error', function () {
+    var gmScope, gmConnector, response;
+
+    before(function() {
+      gmScope = nock('http://gmapi.azurewebsites.net');
+      gmConnector = new GMConnector();
       gmScope
         .post('/getSecurityStatusService', {
           "id": "1239",
@@ -91,27 +128,15 @@ describe('GM connector', function() {
       nock.cleanAll();
     });
 
-    describe('POST /getSecurityStatusService', function() {
-
-      context('when correct id and responseType is passed in', function() {
-        it('should return 200 response with security info', function() {
-          gmConnector._getVehicleInfo({ "id": "1234"})
-            .then(function(response) {
-              expect(response.status).to.equal('200');
-              expect(response.doors.values.length).to.equal(2);
-              expect(response.doors.values[0]).to.have.all.keys('location', 'locked');
-              expect(response.doors.values[1]).to.have.all.keys('location', 'locked');
-            });
-        });
-      });
-
-      context('when incorrect id is passed in', function() {
-        it('should return 404 response with error', function() {
-          gmConnector._getVehicleInfo({ "id": "1239"})
-            .then(function(response) {
-              expect(response.status).to.equal('404');
-            });
-        });
+    context('when incorrect id is passed in', function() {
+      it('should return 404 response', function() {
+        return gmConnector._getSecurityInfo({ "id": "1239"})
+                .then(function() {
+                })
+                .catch(function(error) {
+                  expect(error.error.status).to.equal(404);
+                  expect(error.message).to.equal(errorMessages.oemRequestError);
+                });
       });
     });
   });
